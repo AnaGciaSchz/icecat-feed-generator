bc93aa958304e26b5d78b1c1f39fc84088148d14
Ana María García Sánchez <anamariagarciasanchez@MacBook-Pro-de-Ana.local>
2021-09-01 10:31:33 +0200

    Changing the format of the output to json

--- logic/GetFeed.js
+++ logic/GetFeed.js
@@ -2,15 +2,22 @@ var getData = require("../logic/GetData");
 var fw = require ("../inout/FileWriter")
 
 exports.getFeed = async ()=> {
-fw.write("Name\tSupplier\tCategory\tImages\n");
-var id = 62300701;
+fw.write("[\n");
+var id = 29900045;
+var firstLine = true;
 var i;
 for (i = 0; i<10000;i++){
     var data = await getData.getBasicInformation(id+i);
     if(data.res != "error"){
         console.log(data);
-        fw.write(data.name+"\t"+data.supplier+"\t"+data.category+"\t"+data.image+"\n");
+        if(firstLine){
+            fw.write("{\nname:"+data.name+",\nbrand:"+data.supplier+",\ncategory:"+data.category+",\nimage:"+data.image+"\n}");
+            firstLine = false;
+        }else{
+        fw.write(",\n{\nname:"+data.name+",\nbrand:"+data.supplier+",\ncategory:"+data.category+",\nimage:"+data.image+"\n}");
+        }
     }
 }
+fw.write("\n]");
 return "done";
 }
