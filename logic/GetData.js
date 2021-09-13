0823650b12a1aca1591032bcfdcf05f18f2e063b
Ana María García Sánchez <anamariagarciasanchez@MacBook-Pro-de-Ana.local>
2021-09-13 11:19:15 +0200

    adding more images, subcategories and price (now, the feed is only of electronic products)

--- logic/GetData.js
+++ logic/GetData.js
@@ -1,13 +1,65 @@
 const icecat = require('icecat');
+var uniqid = require('uniqid'); 
+const random = require('random');
 
-exports.getBasicInformation = async (id) => {
+exports.getBasicElectronicInformation = async (id) => {
 try{
-// Language: en, GTIN: 4948570114344 (GTIN: EAN, UPC or GTIN-13/JAN)
 const icecatClient = await new icecat('AnaGciaSchz', '123ana');
 var product = await icecatClient.openCatalog.getProductById('EN', id);
-return {res: "ok",name: product.getTitle(), supplier: product.getSupplier(), category: product.getCategory(), image: product.getImages()[0].HighImg};
+if(isElectronic(product.getCategory())){
+return {res: "ok",id: uniqid(), name: getName(product.getTitle()), supplier: product.getSupplier(), category: "Electronics", subCategory: product.getCategory(), 
+price: getPrice(product.getCategory()),image: getImages(product.getImages())};
+}
+else{
+    return {res: "error"}
+}
 }
 catch(err){
     return {res: "error"}
 }
+}
+
+function getName (name){
+    var rep = '\\'
+    var place = rep+'"'
+    return name.replaceAll('"',place);
+}
+
+function getImages (images){
+    var i = 0;
+    var result ="[\n" ;
+    for (i;i<images.length;i++){
+        result+= "{\n\"HighImg\":\""+ images[i].HighImg+"\",\n \"LowImg\":\""+ images[i].LowImg+"\",\n \"ThumbImg\":\""+ images[i].ThumbImg+"\"}";
+        if(i+1 != images.length){
+            result+=",\n";
+        }
+    }
+    result+="\n]";
+    return result;
+}
+
+function isElectronic(category){
+    console.log(category);
+    var categories = ["Computer Monitors","Computers & Peripherals","Computers",
+        "Warranty & Support","Warranty & Support Extensions","Infotainment",
+        "Telecom & Navigation","Notebooks","Telecommunication Equipment Accessories",
+        "Mobile Communication Device Parts & Accessories","Notebook Parts & Accessories",
+        "All-in-One PCs/Workstations","Notebook Spare Parts", "Computer Cables",
+        "Computer Components","Audio Visual Equipment","Electrical Equipment & Supplies",
+         "Data Storage","NAS & Storage Servers","Tablets","Laser Printers","PCs/Workstations",
+        "Touch Screen Monitors","Notebook Spare Parts","Network Switches","Servers","Smoke Detectors",
+        "Water Detectors","Alarm Lighting", "Graphics Cards","Power Supply Units", "POS Printers",
+        "Internal Hard Drives", "Keyboards", "Smartphones"]
+    return categories.includes(category);
+}
+
+function getPrice(category){
+    var categoryExpensive = ["Computer Monitors","Computers & Peripherals","Computers",
+    "Notebooks","All-in-One PCs/Workstations", "Audio Visual Equipment","Tablets","Laser Printers","PCs/Workstations",
+        "Touch Screen Monitors","Servers", "POS Printers"]
+   if(categoryExpensive.includes(category)){
+       return random.int((min = 300), (max = 1000))+"$";
+   }else{
+    return random.int((min = 100), (max = 500))+"$";
+   }
 }
--- logic/GetFeed.js
+++ logic/GetFeed.js
@@ -7,17 +7,14 @@ var id = 29900045;
 var firstLine = true;
 var i;
 for (i = 0; i<10000;i++){
-    var data = await getData.getBasicInformation(id+i);
+    var data = await getData.getBasicElectronicInformation(id+i);
     if(data.res != "error"){
-        console.log(data);
-        var rep = '\\'
-        var place = rep+'"'
-        var name = data.name.replaceAll('"',place);
+        //console.log(data);
         if(firstLine){
-            fw.write("{\n\"id\":\""+id+i+"\",\n\"name\":\""+name+"\",\n\"brand\":\""+data.supplier+"\",\n\"category\":\""+data.category+"\",\n\"image\":\""+data.image+"\"\n}");
+            fw.write("{\n\"id\":\""+data.id+"\",\n\"name\":\""+data.name+"\",\n\"brand\":\""+data.supplier+"\",\n\"category\":\""+data.category+"\",\n\"subCategory\":\""+data.subCategory+"\",\n\"price\":\""+data.price+"\",\n\"image\":"+data.image+"\n}");
             firstLine = false;
         }else{
-        fw.write(",\n{\n\"id\":\""+id+i+"\",\n\"name\":\""+name+"\",\n\"brand\":\""+data.supplier+"\",\n\"category\":\""+data.category+"\",\n\"image\":\""+data.image+"\"\n}");
+        fw.write(",\n{\n\"id\":\""+data.id+"\",\n\"name\":\""+data.name+"\",\n\"brand\":\""+data.supplier+"\",\n\"category\":\""+data.category+"\",\n\"subCategory\":\""+data.subCategory+"\",\n\"price\":\""+data.price+"\",\n\"image\":"+data.image+"\n}");
         }
     }
 }
