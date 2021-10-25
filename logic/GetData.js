5ebb77cc65dd0f6c7dd1077944852fc7d3b24bcd
Ana María García Sánchez <anamariagarciasanchez@MacBook-Pro-de-Ana.local>
2021-10-25 14:12:27 +0200

    Adding info

--- logic/GetData.js
+++ logic/GetData.js
@@ -14,10 +14,21 @@ if(isElectronic(product.getCategory())){
     catch(err){
 
     }
+    var priceNew = getPrice(product.getCategory())
+    var priceOld = random.int((min = priceNew+50), (max = priceNew+200));
+
 return {res: "ok",id: uniqid(),ean: eanValue, name: quitQuotationMarks(product.getTitle()), 
 supplier: product.getSupplier(), category: "Electronics", subCategory: product.getCategory(), 
-price: getPrice(product.getCategory()),description: getDescription(product), height: getHeight(product),
-weight: getWeight(product),power: getPower(product), color: getColour(product), image: getImages(product.getImages())};
+price: priceNew, displayDiag: getDisplayDiagonal(product),numKeypad: getNumericKeypad(product),
+oldPrice : priceOld,purpose: getPurpose(product), description: getDescription(product), 
+height: getHeight(product),weight: getWeight(product),OSInstalled: getOperatingSystemInstalled(product),
+processorFreq: getProcessorFrequency(product),led: getLED(product), displayRes: getDisplayResolution(product),
+disponible: getDisponibility(),hdmiPorts: getHDMIPorts(product), wifi: getWifi(product),
+processorModel: getProcessorModel(product),internalMemory: getInternalMemory(product), ethernetLan: getLAN(product),
+pointingDevice: getPointingDevice(product), bluetooth: getBluetooth(product),
+power: getPower(product), OSArquitecture: getOperatingsSystemArchitecture(product),
+color: getColour(product),  rating: random.float((min = 1.5), (max = 5)), 
+image: getImages(product.getImages())};
 }
 else{
     return {res: "error"}
@@ -32,6 +43,11 @@ function quitQuotationMarks (string){
     var rep = '\\'
     var place = rep+'"'
     return string.replaceAll('"',place);
+}
+
+function getDisponibility(){
+    var probability= random.int((min = 40), (max = 100));
+    return probability>50?"Yes":"No";
 }
 
 function getImages (images){
@@ -97,6 +113,206 @@ function getWeight(product){
     var i = 0;
     for(i;i<specifications.length;i++){
         if(specifications[i].name.includes("Weight")){
+            return specifications[i].value;
+        }
+    }
+    return 0;
+}
+
+function getOperatingsSystemArchitecture(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Operating system architecture")){
+            return specifications[i].value;
+        }
+    }
+    return "";
+}
+
+function getPointingDevice(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Pointing device")){
+            return specifications[i].value;
+        }
+    }
+    return "";
+}
+
+function getBluetooth(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Bluetooth")){
+            var result = specifications[i].value;
+            if(result =="Y"){
+                return "Yes"
+            }
+            else if(result == "N"){
+                return "No"
+            }
+        }
+    }
+    return "";
+}
+
+function getInternalMemory(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Internal memory")){
+            return specifications[i].value;
+        }
+    }
+    return 0;
+}
+
+function getProcessorModel(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Processor model")){
+            return specifications[i].value;
+        }
+    }
+    return "";
+}
+
+function getPurpose(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Purpose")){
+            return specifications[i].value;
+        }
+    }
+    return "";
+}
+
+function getNumericKeypad(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Numeric keypad")){
+            var result = specifications[i].value;
+            if(result =="Y"){
+                return "Yes"
+            }
+            else if(result == "N"){
+                return "No"
+            }
+        }
+    }
+    return "";
+}
+function getLED(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("LED")){
+            var result = specifications[i].value;
+            if(result =="Y"){
+                return "Yes"
+            }
+            else if(result == "N"){
+                return "No"
+            }
+        }
+    }
+    return "";
+}
+
+function getProcessorFrequency(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Processor frequency")){
+            return specifications[i].value;
+        }
+    }
+    return 0;
+}
+
+function getDisplayDiagonal(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Display diagonal")){
+            return specifications[i].value;
+        }
+    }
+    return 0;
+}
+
+function getOperatingSystemInstalled(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Operating system installed")){
+            var result = specifications[i].value;
+            if(result =="Y"){
+                return "Yes"
+            }
+            else if(result == "N"){
+                return "No"
+            }
+        }
+    }
+    return "";
+}
+
+function getLAN(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Ethernet LAN")){
+            var result = specifications[i].value;
+            if(result =="Y"){
+                return "Yes"
+            }
+            else if(result == "N"){
+                return "No"
+            }
+        }
+    }
+    return "";
+}
+
+function getWifi(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Wi-Fi")){
+            var result = specifications[i].value;
+            if(result =="Y"){
+                return "Yes"
+            }
+            else if(result == "N"){
+                return "No"
+            }
+        }
+    }
+    return "";
+}
+
+function getDisplayResolution(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("Display resolution")){
+            return specifications[i].value;
+        }
+    }
+    return "";
+}
+
+function getHDMIPorts(product){
+    var specifications = product.getSpecifications();
+    var i = 0;
+    for(i;i<specifications.length;i++){
+        if(specifications[i].name.includes("HDMI ports quantity")){
             return specifications[i].value;
         }
     }
--- logic/GetFeed.js
+++ logic/GetFeed.js
@@ -10,20 +10,48 @@ for (i = 0; i<10000;i++){
     var data = await getData.getBasicElectronicInformation(id+i);
     if(data.res != "error"){
         if(firstLine){
-            fw.write("{\n\"id\":\""+data.id+"\",\n\"EAN\":\""+data.ean+"\",\n\"name\":\""+data.name+"\",\n\"description\":\""+
+            fw.write("{\n\"id\":\""+data.id+"\",\n\"EAN\":\""+data.ean+"\",\n\"name\":\""+data.name+
+            "\",\n\"description\":\""+
             data.description+"\",\n\"brand\":\""+data.supplier+"\",\n\"category\":\""+
-            data.category+"\",\n\"subCategory\":\""+data.subCategory+"\",\n\"price\":\""+
-            data.price+"\",\n\"height\":\""+data.height+"\",\n\"weight\":\""+
-            data.weight+"\",\n\"power\":\""+data.power+"\",\n\"color\":\""+data.color+"\",\n\"image\":"+
-            data.image+",\n\"language\":\"en\"\n}");
+            data.category+"\",\n\"subCategory\":\""+data.subCategory+"\",\n\"pricePerUnit\":\""+
+            data.price+"\",\n\"oldPrice\":\""+data.oldPrice+"\",\n\"OSArquitecture\":\""+
+            data.OSArquitecture+"\",\n\"height\":\""+data.height+"\",\n\"weight\":\""+
+            data.weight+"\",\n\"led\":\""+data.led+"\",\n\"processorFreq\":\""+
+            data.processorFreq+"\",\n\"processorModel\":\""+
+            data.processorModel+"\",\n\"internalMemory\":\""+data.internalMemory+"\",\n\"bluetooth\":\""+
+            data.bluetooth+"\",\n\"power\":\""+data.power+"\",\n\"displayDiag\":\""+data.displayDiag+
+            "\",\n\"disponible\":\""+data.disponible+
+            "\",\n\"wifi\":\""+data.wifi+
+            "\",\n\"ethernetLan\":\""+data.ethernetLan+
+            "\",\n\"displayRes\":\""+data.displayRes+
+            "\",\n\"hdmiPorts\":\""+data.hdmiPorts+
+            "\",\n\"OSInstalled\":\""+data.OSInstalled+
+            "\",\n\"pointingDevice\":\""+data.pointingDevice
+            +"\",\n\"color\":\""+data.color+"\",\n\"rating\":\""+
+            data.rating+"\",\n\"numKeypad\":\""+data.numKeypad+"\",\n\"purpose\":\""
+            +data.purpose+"\",\n\"image\":"+data.image+",\n\"language\":\"en\"\n}");
             firstLine = false;
         }else{
-            fw.write(",\n{\n\"id\":\""+data.id+"\",\n\"EAN\":\""+data.ean+"\",\n\"name\":\""+data.name+"\",\n\"description\":\""+
+            fw.write(",\n{\n\"id\":\""+data.id+"\",\n\"EAN\":\""+data.ean+"\",\n\"name\":\""+data.name+
+            "\",\n\"description\":\""+
             data.description+"\",\n\"brand\":\""+data.supplier+"\",\n\"category\":\""+
-            data.category+"\",\n\"subCategory\":\""+data.subCategory+"\",\n\"price\":\""+
-            data.price+"\",\n\"height\":\""+data.height+"\",\n\"weight\":\""+
-            data.weight+"\",\n\"power\":\""+data.power+"\",\n\"color\":\""+data.color+"\",\n\"image\":"+
-            data.image+",\n\"language\":\"en\"\n}");
+            data.category+"\",\n\"subCategory\":\""+data.subCategory+"\",\n\"pricePerUnit\":\""+
+            data.price+"\",\n\"oldPrice\":\""+data.oldPrice+"\",\n\"OSArquitecture\":\""+
+            data.OSArquitecture+"\",\n\"height\":\""+data.height+"\",\n\"weight\":\""+
+            data.weight+"\",\n\"led\":\""+data.led+"\",\n\"processorFreq\":\""+
+            data.processorFreq+"\",\n\"processorModel\":\""+
+            data.processorModel+"\",\n\"internalMemory\":\""+data.internalMemory+"\",\n\"bluetooth\":\""+
+            data.bluetooth+"\",\n\"power\":\""+data.power+"\",\n\"displayDiag\":\""+data.displayDiag+
+            "\",\n\"disponible\":\""+data.disponible+
+            "\",\n\"wifi\":\""+data.wifi+
+            "\",\n\"ethernetLan\":\""+data.ethernetLan+
+            "\",\n\"displayRes\":\""+data.displayRes+
+            "\",\n\"hdmiPorts\":\""+data.hdmiPorts+
+            "\",\n\"OSInstalled\":\""+data.OSInstalled+
+            "\",\n\"pointingDevice\":\""+data.pointingDevice
+            +"\",\n\"color\":\""+data.color+"\",\n\"rating\":\""+
+            data.rating+"\",\n\"numKeypad\":\""+data.numKeypad+"\",\n\"purpose\":\""+
+            data.purpose+"\",\n\"image\":"+data.image+",\n\"language\":\"en\"\n}");
         }
     }
 }
